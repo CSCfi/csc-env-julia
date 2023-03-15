e7d3b78211f529edb4a12dca949d8cdcc73081f9
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-15 09:52:43 +0200

    improvements

--- modulefiles/ijulia.lua
+++ modulefiles/ijulia.lua
@@ -5,10 +5,10 @@ Julia kernel for Jupyter.
 depends_on("julia/1.8.5")
 
 local JUPYTER_BIN = "/appl/soft/math/julia/jupyter-env/bin"
-local JULIA_LOAD_PATH = (os.getenv("JULIA_LOAD_PATH") or "") .. ":/appl/soft/math/julia/depot/environments/v1.8_ijulia"
+local JULIA_LOAD_PATH = ":/appl/soft/math/julia/depot/environments/v1.8_ijulia"
 
 prepend_path("PATH", JUPYTER_BIN)
-setenv("JULIA_LOAD_PATH", JULIA_LOAD_PATH)
+setenv("JULIA_LOAD_PATH", (os.getenv("JULIA_LOAD_PATH") or "") .. JULIA_LOAD_PATH)
 
 if (mode() == "load") then
     LmodMessage("Julia kernel for Jupyter loaded")
--- modulefiles/julia.lua
+++ modulefiles/julia.lua
@@ -6,8 +6,8 @@ depends_on("StdEnv")
 
 local JULIA_VERSION = "1.8.5"
 local JULIA_DIR = "/appl/soft/math/julia/julia-" .. JULIA_VERSION
-local JULIA_DEPOT_PATH = (os.getenv("JULIA_DEPOT_PATH") or "") .. ":/appl/soft/math/julia/depot"
-local JULIA_LOAD_PATH = (os.getenv("JULIA_LOAD_PATH") or "") .. ":/appl/soft/math/julia/depot/environments/v1.8_shared"
+local JULIA_DEPOT_PATH = ":/appl/soft/math/julia/depot"
+local JULIA_LOAD_PATH = ":/appl/soft/math/julia/depot/environments/v1.8_shared"
 local APPLDIR = pathJoin(JULIA_DIR, "usr/bin")
 local LIBDIR = pathJoin(JULIA_DIR, "usr/lib")
 local MANDIR = pathJoin(JULIA_DIR, "usr/share/man")
@@ -15,8 +15,8 @@ local MANDIR = pathJoin(JULIA_DIR, "usr/share/man")
 prepend_path("LD_LIBRARY_PATH", LIBDIR)
 prepend_path("PATH", APPLDIR)
 prepend_path("MANPATH", MANDIR)
-setenv("JULIA_DEPOT_PATH", JULIA_DEPOT_PATH)
-setenv("JULIA_LOAD_PATH", JULIA_LOAD_PATH)
+setenv("JULIA_DEPOT_PATH", (os.getenv("JULIA_DEPOT_PATH") or "") .. JULIA_DEPOT_PATH)
+setenv("JULIA_LOAD_PATH", (os.getenv("JULIA_LOAD_PATH") or "") .. JULIA_LOAD_PATH)
 
 if (mode() == "load") then
     LmodMessage("julia", JULIA_VERSION, "loaded")
