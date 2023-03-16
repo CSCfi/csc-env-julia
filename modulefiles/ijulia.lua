1e8d0f9d0c6f575388fe73d70b3988bad1d11024
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-16 13:21:37 +0200

    updated modulefiles

--- modulefiles/ijulia.lua
+++ modulefiles/ijulia.lua
@@ -2,11 +2,9 @@ help([[
 Julia kernel for Jupyter.
 ]])
 
-depends_on("julia/1.8.5")
-
 local JUPYTER_BIN = "/appl/soft/math/julia/jupyter-env/bin"
 local JULIA_LOAD_PATH = ":/appl/soft/math/julia/depot/environments/v1.8_ijulia"
 
+depends_on("julia/1.8.5")
 prepend_path("PATH", JUPYTER_BIN)
 setenv("JULIA_LOAD_PATH", (os.getenv("JULIA_LOAD_PATH") or "") .. JULIA_LOAD_PATH)
-
--- modulefiles/julia.lua
+++ modulefiles/julia.lua
@@ -2,8 +2,6 @@ help([[
 Julia language and dependencies.
 ]])
 
-depends_on("StdEnv")
-
 local JULIA_VERSION = "1.8.5"
 local JULIA_DIR = "/appl/soft/math/julia/julia-" .. JULIA_VERSION
 local JULIA_DEPOT_PATH = ":/appl/soft/math/julia/depot"
@@ -12,9 +10,9 @@ local APPLDIR = pathJoin(JULIA_DIR, "usr/bin")
 local LIBDIR = pathJoin(JULIA_DIR, "usr/lib")
 local MANDIR = pathJoin(JULIA_DIR, "usr/share/man")
 
+depends_on("StdEnv")
 prepend_path("LD_LIBRARY_PATH", LIBDIR)
 prepend_path("PATH", APPLDIR)
 prepend_path("MANPATH", MANDIR)
 setenv("JULIA_DEPOT_PATH", (os.getenv("JULIA_DEPOT_PATH") or "") .. JULIA_DEPOT_PATH)
 setenv("JULIA_LOAD_PATH", (os.getenv("JULIA_LOAD_PATH") or "") .. JULIA_LOAD_PATH)
-
