f30d92f784bebc35017f631dc9895e2e807b6d4f
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-16 14:38:25 +0200

    blas and cuda

--- test/blas/batch-openblas.sh
+++ test/blas/batch-openblas.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=threads
+#SBATCH --job-name=openblas
 #SBATCH --account=project_2001659
 #SBATCH --partition=test
 #SBATCH --time=00:15:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=4000
 module load julia/1.8.5
-srun julia --project=. -t "$SLURM_CPUS_PER_TASK" benchmark.jl
+srun julia --project=. -t "$SLURM_CPUS_PER_TASK" benchmark.jl -n 10000
--- test/cuda/benchmark.jl
+++ test/cuda/benchmark.jl
@@ -5,7 +5,5 @@ using CUDA
 Random.seed!(802365)
 
 A = rand(10_000, 10_000)
-@btime $A * $A
-
 A_d = CuArray(A)
 @btime $A_d * $A_d
