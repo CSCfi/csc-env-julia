fa4c9e16d48e83d54268bbcfaed5c84f7c90e581
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-17 14:03:00 +0200

    show

--- test/clustermanagers/test.jl
+++ test/clustermanagers/test.jl
@@ -3,10 +3,9 @@ using ClusterManagers
 
 n = parse(Int, ENV["SLURM_NTASKS"])
 addprocs(SlurmManager(n), topology=:master_worker)
-task(i) = @spawnat(i (gethostname(), getpid()))
-futures = [task(i) for i in workers()]
-outputs = fetch.(futures)
-@show outputs
+task(i) = @spawnat i (myid(), gethostname(), getpid())
+@show futures = [task(i) for i in workers()]
+@show outputs = fetch.(futures)
 
 # The Slurm resource allocation is released when all the workers have exited
 rmprocs.(workers())
