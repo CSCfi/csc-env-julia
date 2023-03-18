34170d2937f95c086a60560d8b5509d634a0c595
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-18 13:30:02 +0200

    packages

--- packages/core.sh
+++ packages/core.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 module purge
-module load StdEnv
-module load openmpi/4.1.4
-module load cuda/11.7.0
-module load julia/1.8.5
+module load gcc/11.3.0 openmpi/4.1.4 cuda/11.7.0 julia/1.8.5
+#TODO: should we use "openmpi/4.1.4-cuda" for CUDA-aware MPI?
 
 # Load packages from default locations
 unset JULIA_LOAD_PATH
