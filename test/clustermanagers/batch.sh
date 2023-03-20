7acbec4c37ad6dc8c2cb95e122da851319f213ad
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-20 15:41:50 +0200

    use single node

--- test/clustermanagers/batch.sh
+++ test/clustermanagers/batch.sh
@@ -3,6 +3,7 @@
 #SBATCH --account=project_2001659
 #SBATCH --partition=test
 #SBATCH --time=00:15:00
+#SBATCH --nodes=1
 #SBATCH --ntasks=2
 #SBATCH --mem=1000
 module load julia/1.8.5
--- test/clustermanagers/test.jl
+++ test/clustermanagers/test.jl
@@ -4,7 +4,7 @@ using ClusterManagers
 n = parse(Int, ENV["SLURM_NTASKS"])
 addprocs(SlurmManager(n), topology=:master_worker)
 task(i) = @spawnat i (myid(), gethostname(), getpid())
-@show futures = [task(i) for i in workers()]
+futures = [task(i) for i in workers()]
 @show outputs = fetch.(futures)
 
 # The Slurm resource allocation is released when all the workers have exited
