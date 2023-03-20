9fc09ff0c2f5ae4be9cd8f1923cb99a5d7685fde
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-20 13:41:14 +0200

    julia-test modulefiles

--- test/openmpi-cuda/batch.sh
+++ test/openmpi-cuda/batch.sh
@@ -8,5 +8,5 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4000
 #SBATCH --gres=gpu:v100:2
-module load julia/1.8.5-openmpi-cuda
+module load julia-test/1.8.5-openmpi-cuda
 srun julia --project=. test.jl
