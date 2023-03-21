1abd1488242819f5f314e79c1e3d78d06b67065c
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-21 10:19:45 +0200

    pin only major version

--- modulefiles/julia/1.8.5-cuda.lua
+++ modulefiles/julia/1.8.5-cuda.lua
@@ -1,2 +1,2 @@
-depends_on("gcc/11.3.0", "cuda/11.7.0", "julia/1.8.5")
+depends_on("gcc/11", "cuda/11", "julia/1.8.5")
 setenv("JULIA_CUDA_USE_MEMORY_POOL", "none")
--- modulefiles/julia/1.8.5-openmpi.lua
+++ modulefiles/julia/1.8.5-openmpi.lua
@@ -1,1 +1,1 @@
-depends_on("gcc/11.3.0", "openmpi/4.1.4", "julia/1.8.5")
+depends_on("gcc/11", "openmpi/4", "julia/1.8.5")
