89fea934387daca73b694ae023b3ff996b84577f
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-21 09:23:23 +0200

    refactoring

---
+++ install/packages-mahti.jl
@@ -0,0 +1,13 @@
+using Pkg
+Pkg.add(name="CUDA", version="4.0.1")
+Pkg.add(name="MPI", version="0.20.8")
+Pkg.add(name="MPIPreferences")
+Pkg.add(name="ClusterManagers")
+
+using CUDA
+CUDA.set_runtime_version!("local")
+
+using MPIPreferences
+MPIPrererences.use_system_binary()
+
+Pkg.instantiate()
