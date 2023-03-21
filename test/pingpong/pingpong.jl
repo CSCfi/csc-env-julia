7bd00b9910653606d5cc30e560230256986a122c
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-21 15:31:23 +0200

    pingpong

--- test/pingpong/pingpong.jl
+++ test/pingpong/pingpong.jl
@@ -24,8 +24,16 @@ function pingpong(T, bufsize, iters)
 end
 
 MPI.Init()
+comm = MPI.COMM_WORLD
+rank = MPI.Comm_rank(comm)
+
+# perform a warmup iteration to avoid measuring compilation time
+pingpong(Float64, 10, 2)
+
 for i = 1:6
-    t = pingpong(Float64, 10^i, 1000)
-    println("$(i),$(t)")
+    t = pingpong(Float64, 10^i, 10000)
+    if rank == 0
+        println("10^$(i), $(t)")
+    end
 end
 
