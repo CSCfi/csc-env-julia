903124a7e2f5f5969c73bcaf1e789ab6fe26c982
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-22 08:39:37 +0200

    mpi benchmarks

---
+++ test/mpi_benchmarks/puhti.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=mpi_benchmarks
+#SBATCH --account=project_2001659
+#SBATCH --partition=test
+#SBATCH --time=00:15:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8000
+module load julia/1.8.5-openmpi
+srun julia --project=. benchmark.jl
