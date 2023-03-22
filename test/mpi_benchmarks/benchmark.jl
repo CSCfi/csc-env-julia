644036fc1d60f0b164e4079f747019b4f50710a9
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-22 08:45:56 +0200

    added all benchmarks

--- test/mpi_benchmarks/benchmark.jl
+++ test/mpi_benchmarks/benchmark.jl
@@ -1,2 +1,16 @@
 using MPIBenchmarks
+
+# Collective benchmarks
+benchmark(IMBAllreduce())
+benchmark(IMBAlltoall())
+benchmark(IMBGatherv())
+benchmark(IMBReduce())
+benchmark(OSUAllreduce())
+benchmark(OSUAlltoall())
+benchmark(OSUReduce())
+
+# Point-to-point benchmarks.
+# NOTE: they require exactly two MPI processes.
+benchmark(IMBPingPong())
+benchmark(IMBPingPing())
 benchmark(OSULatency())
