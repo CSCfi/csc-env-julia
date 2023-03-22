fd7c76ae89438c529d5b5e110f66ba577bb9a12a
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-22 10:28:18 +0200

    simplified modulefiles

--- modulefiles/julia-packages/1.8.5.lua
+++ modulefiles/julia-packages/1.8.5.lua
@@ -1,7 +1,8 @@
-depends_on("julia/1.8.5")
+depends_on("gcc/11", "openmpi/4", "cuda/11", "julia/1.8.5")
+
 unsetenv("JULIA_LOAD_PATH")
 setenv("JULIA_DEPOT_PATH", "/appl/soft/math/julia/depot")
-setenv("JULIA_PROJECT", "/appl/soft/math/julia/depot/environments/v1.8_ijulia")
+setenv("JULIA_PROJECT", "/appl/soft/math/julia/depot/environments/v1.8_shared")
 
 -- Diretory for the Jupyter kernel
 setenv("JUPYTER_DATA_DIR", "/appl/soft/math/julia/jupyter-env/share/jupyter")
