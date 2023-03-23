4f76b85902dd25a63bda1edbebd9970b336f598c
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-23 13:28:35 +0200

    myid

--- test/distributed/test.jl
+++ test/distributed/test.jl
@@ -4,6 +4,7 @@ n = parse(Int, ENV["SLURM_NTASKS_PER_NODE"])
 addprocs(n)
 task(i) = @spawnat i (myid(), gethostname(), getpid())
 futures = [task(i) for i in workers()]
+@show myid()
 @show outputs = fetch.(futures)
 
 # The Slurm resource allocation is released when all the workers have exited
