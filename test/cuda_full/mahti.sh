4e002c7b311844fbfa78527946575ea14eeff04e
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-27 11:26:45 +0300

    mahti batch script

---
+++ test/cuda_full/mahti.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=cuda_full
+#SBATCH --account=project_2001659
+#SBATCH --partition=gputest
+#SBATCH --time=00:15:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:a100:1
+module load julia/1.8.5
+export JULIA_NUM_THREADS="$SLURM_CPUS_PER_TASK"
+srun julia --project=. test.jl
