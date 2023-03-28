b8470bfde50cd8f07038795639fdb3061026d70e
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-28 09:16:43 +0300

    set load and depot paths explicitly

--- modulefiles/julia/1.8.5.lua
+++ modulefiles/julia/1.8.5.lua
@@ -2,18 +2,24 @@ help([[
 Julia language and dependencies.
 ]])
 
+local JULIA_APPLDIR = "/appl/soft/math/julia"
 local JULIA_VERSION = "1.8.5"
-local JULIA_DIR = "/appl/soft/math/julia/julia-" .. JULIA_VERSION
-local JULIA_DEPOT_PATH = "::/appl/soft/math/julia/depot"
-local JULIA_LOAD_PATH = "::/appl/soft/math/julia/depot/environments/v1.8_shared"
-local JULIA_BINDIR = pathJoin(JULIA_DIR, "usr/bin")
-local JULIA_LIBDIR = pathJoin(JULIA_DIR, "usr/lib")
-local JULIA_MANDIR = pathJoin(JULIA_DIR, "usr/share/man")
+local JULIA_DIR = pathJoin(JULIA_APPLDIR, "julia-" .. JULIA_VERSION)
+local HOME = os.getenv("HOME")
 
 depends_on("gcc/11", "openmpi/4", "cuda/11")
-prepend_path("LD_LIBRARY_PATH", JULIA_LIBDIR)
-prepend_path("PATH", JULIA_BINDIR)
-prepend_path("MANPATH", JULIA_MANDIR)
-setenv("JULIA_DEPOT_PATH", (os.getenv("JULIA_DEPOT_PATH") or "") .. JULIA_DEPOT_PATH)
-setenv("JULIA_LOAD_PATH", (os.getenv("JULIA_LOAD_PATH") or "") .. JULIA_LOAD_PATH)
+
+prepend_path("LD_LIBRARY_PATH", pathJoin(JULIA_DIR, "usr/lib"))
+prepend_path("PATH", pathJoin(JULIA_DIR, "usr/bin"))
+prepend_path("MANPATH", pathJoin(JULIA_DIR, "usr/share/man"))
+
+append_path("JULIA_DEPOT_PATH", pathJoin(HOME, ".julia"))
+append_path("JULIA_DEPOT_PATH", pathJoin(JULIA_APPLDIR, "depot"))
+append_path("JULIA_DEPOT_PATH", pathJoin(JULIA_DIR, "usr/share/julia"))
+
+append_path("JULIA_LOAD_PATH", "@")
+append_path("JULIA_LOAD_PATH", "@v#.#")
+append_path("JULIA_LOAD_PATH", "@stdlib")
+append_path("JULIA_LOAD_PATH", pathJoin(JULIA_APPLDIR, "depot/environments/v1.8_shared"))
+
 setenv("JULIA_CUDA_USE_MEMORY_POOL", "none")
