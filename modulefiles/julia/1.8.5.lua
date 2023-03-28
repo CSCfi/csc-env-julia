1db1a0d795831eab0b1a7c28aa848289b1c5eaf0
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-28 10:09:04 +0300

    some documentation

--- modulefiles/julia/1.8.5.lua
+++ modulefiles/julia/1.8.5.lua
@@ -5,21 +5,33 @@ Julia language and dependencies.
 local JULIA_APPLDIR = "/appl/soft/math/julia"
 local JULIA_VERSION = "1.8.5"
 local JULIA_DIR = pathJoin(JULIA_APPLDIR, "julia-" .. JULIA_VERSION)
-local HOME = os.getenv("HOME")
 
+-- Dependencies for Julia and shared packages.
+-- MPI.jl depends on Open MPI
+-- CUDA.jl depends on CUDA.
 depends_on("gcc/11", "openmpi/4", "cuda/11")
 
+-- Path to the Julia library files.
 prepend_path("LD_LIBRARY_PATH", pathJoin(JULIA_DIR, "usr/lib"))
+
+-- Path to the Julia binary.
 prepend_path("PATH", pathJoin(JULIA_DIR, "usr/bin"))
+
+-- Path to the Julia man pages.
 prepend_path("MANPATH", pathJoin(JULIA_DIR, "usr/share/man"))
 
-append_path("JULIA_DEPOT_PATH", pathJoin(HOME, ".julia"))
+-- We set the depot path explicitly such that it is easier to modify.
+-- https://docs.julialang.org/en/v1/base/constants/#Base.DEPOT_PATH
+append_path("JULIA_DEPOT_PATH", pathJoin(os.getenv("HOME"), ".julia"))
 append_path("JULIA_DEPOT_PATH", pathJoin(JULIA_APPLDIR, "depot"))
 append_path("JULIA_DEPOT_PATH", pathJoin(JULIA_DIR, "usr/share/julia"))
 
+-- We set load path explicitly such that it is easier to modify.
+-- https://docs.julialang.org/en/v1/base/constants/#Base.LOAD_PATH
 append_path("JULIA_LOAD_PATH", "@")
 append_path("JULIA_LOAD_PATH", "@v#.#")
 append_path("JULIA_LOAD_PATH", "@stdlib")
 append_path("JULIA_LOAD_PATH", pathJoin(JULIA_APPLDIR, "depot/environments/v1.8_shared"))
 
+-- Disable memory pool for CUDA.jl
 setenv("JULIA_CUDA_USE_MEMORY_POOL", "none")
