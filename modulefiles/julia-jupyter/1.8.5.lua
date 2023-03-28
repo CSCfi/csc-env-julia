a944132a5039e2778c9cbef0952492dac74d1c10
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-28 09:32:33 +0300

    append instead of prepend

--- modulefiles/julia-jupyter/1.8.5.lua
+++ modulefiles/julia-jupyter/1.8.5.lua
@@ -5,4 +5,4 @@ Julia kernel for Jupyter.
 local JUPYTER_BIN = "/appl/soft/math/julia/jupyter-env/bin"
 
 depends_on("julia/1.8.5")
-prepend_path("PATH", JUPYTER_BIN)
+append_path("PATH", JUPYTER_BIN)
