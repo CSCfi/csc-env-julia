6593b4f28ed95c619af661c5fe1143f0afba1ffa
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-03-30 09:39:10 +0300

    julia-jupyter module

--- modulefiles/julia-jupyter/1.8.5.lua
+++ modulefiles/julia-jupyter/1.8.5.lua
@@ -2,5 +2,9 @@ help([[
 Private Jupyter Notebook and Lab for Julia on Open OnDemand.
 ]])
 
+-- Set path to Jupyter
 local JULIA_APPLDIR = "/appl/soft/math/julia"
 prepend_path("PATH", pathJoin(JULIA_APPLDIR, "jupyter-env/bin"))
+
+-- Load Julia
+depends_on("julia/1.8.5")
