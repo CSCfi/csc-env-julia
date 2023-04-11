efa4e921a7b7da22e40b6dc35f536c3f2752978d
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-04-11 10:12:38 +0300

    fixed batch script

--- test/julia/puhti.sh
+++ test/julia/puhti.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=test_julia
 #SBATCH --account=project_2001659
-#SBATCH --partition=test
-#SBATCH --time=00:15:00
+#SBATCH --partition=small
+#SBATCH --time=00:40:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=20
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=2000
 module load julia/1.8.5
-export JULIA_CPU_THREADS="$((SLURM_TASKS_PER_NODE - 1))"
-export JULIA_NUM_THREADS="$SLURM_CPUS_PER_TASK"
-julia --project=. test.jl
+export JULIA_CPU_THREADS=$((SLURM_CPUS_PER_TASK - 1))
+export JULIA_NUM_THREADS=$SLURM_CPUS_PER_TASK
+srun julia --project=. test.jl
