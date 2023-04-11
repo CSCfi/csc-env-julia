8d43651e0c2e8d0637485677ec8ce865e3fe39ed
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-04-11 08:36:12 +0300

    julia tests

--- test/julia/puhti.sh
+++ test/julia/puhti.sh
@@ -8,4 +8,6 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2000
 module load julia/1.8.5
-srun julia --project=. test.jl
+export JULIA_CPU_THREADS="$((SLURM_TASKS_PER_NODE - 1))"
+export JULIA_NUM_THREADS="$SLURM_CPUS_PER_TASK"
+julia --project=. test.jl
--- test/julia/test.jl
+++ test/julia/test.jl
@@ -1,3 +1,8 @@
-julia_appldir = dirname(Sys.BINDDIR)
-testfile = joinpath(julia_appldir, "share", "julia", "test", "runtest.jl")
-include("testfile")
+# Find Julia application directory
+JULIA_APPLDIR = dirname(Sys.BINDIR)
+
+# Path to the file to that contains tests for Julia's Base and Stdlib
+testfile = joinpath(JULIA_APPLDIR, "share", "julia", "test", "runtests.jl")
+
+# Include the file to run the tests
+include(testfile)
