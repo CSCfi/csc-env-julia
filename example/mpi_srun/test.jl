292315aee7d6495958f1701464dc6f0dd08efd04
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-04-12 10:01:10 +0300

    print hostname and pid

--- example/mpi_srun/test.jl
+++ example/mpi_srun/test.jl
@@ -3,5 +3,5 @@ MPI.Init()
 comm = MPI.COMM_WORLD
 rank = MPI.Comm_rank(comm)
 size = MPI.Comm_size(comm)
-println("Hello from process $(rank) out of $(size)")
+println("Hello from process $(rank) out of $(size). Hostname $(gethostname()) and pid $(getpid()).")
 MPI.Barrier(comm)
