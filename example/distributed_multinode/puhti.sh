e56d6288653478da9b0a858067ba4ff5292f88a5
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-04-18 14:09:26 +0300

    use multiple cpus per node

--- example/distributed_multinode/puhti.sh
+++ example/distributed_multinode/puhti.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=00:05:00
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4000
 #SBATCH --nodelist=r07c[05-06]
 module load julia/1.8.5
--- example/distributed_multinode/test.jl
+++ example/distributed_multinode/test.jl
@@ -1,7 +1,11 @@
 using Distributed
 
-# TODO: parse from ENV["SLURM_JOB_NODELIST"]
-addprocs([("r07c05.bullx", Sys.CPU_THREADS), ("r07c06.bullx", Sys.CPU_THREADS)]; tunnel=true)
+# TODO: parse from nodes from ENV["SLURM_JOB_NODELIST"]
+# TODO: add processes on the local node with LocalManager
+# TODO: add processes on the other nodes with SSHManager
+addprocs(Sys.CPU_THREADS)
+addprocs([("r07c06.bullx", Sys.CPU_THREADS)]; tunnel=true)
+#addprocs([("r07c05.bullx", Sys.CPU_THREADS), ("r07c06.bullx", Sys.CPU_THREADS)]; tunnel=true)
 
 @everywhere function task()
     return (myid(), gethostname(), getpid())
@@ -10,7 +14,6 @@ end
 futures = [@spawnat i task() for i in workers()]
 outputs = fetch.(futures)
 
-println("nprocs: ", nprocs())
 println("task:", task())
 println.("outputs: ", outputs)
 
