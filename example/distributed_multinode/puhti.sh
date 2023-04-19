a8c6c13cd0133dc815bbf06f41344d533c8852fd
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-04-19 08:19:18 +0300

    determine nodenames automatically

--- example/distributed_multinode/puhti.sh
+++ example/distributed_multinode/puhti.sh
@@ -7,6 +7,5 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4000
-#SBATCH --nodelist=r07c[05-06]
 module load julia/1.8.5
 julia --project=. test.jl
--- example/distributed_multinode/test.jl
+++ example/distributed_multinode/test.jl
@@ -1,20 +1,41 @@
 using Distributed
 
-# TODO: parse from nodes from ENV["SLURM_JOB_NODELIST"]
-# TODO: add processes on the local node with LocalManager
-# TODO: add processes on the other nodes with SSHManager
-addprocs(Sys.CPU_THREADS)
-addprocs([("r07c06.bullx", Sys.CPU_THREADS)]; tunnel=true)
-#addprocs([("r07c05.bullx", Sys.CPU_THREADS), ("r07c06.bullx", Sys.CPU_THREADS)]; tunnel=true)
+# List of nodes allocated by Slurm.
+nodes = readlines(`scontrol show hostnames $(ENV["SLURM_JOB_NODELIST"])`)
+
+# Host name of the master process.
+local_node = gethostname()
+
+# Environment variable that we pass to the worker processes.
+proc_env = ["JULIA_NUM_THREADS"=>"1", "JULIA_CPU_THREADS"=>"1"]
+
+# Add processes on the local node with LocalManager
+addprocs(Sys.CPU_THREADS;
+         env=proc_env,
+         enable_threaded_blas=false)
+
+# Add processes on the other nodes with SSHManager
+addprocs([(node, Sys.CPU_THREADS) for node in nodes if node != local_node];
+         tunnel=true, env=proc_env,
+         enable_threaded_blas=false)
 
 @everywhere function task()
-    return (myid(), gethostname(), getpid())
+    (
+        id=myid(),
+        hostname=gethostname(),
+        pid=getpid(),
+        nthreads=Base.Threads.nthreads(),
+        cputhreads=Sys.CPU_THREADS
+    )
 end
 
 futures = [@spawnat i task() for i in workers()]
 outputs = fetch.(futures)
 
-println("task:", task())
-println.("outputs: ", outputs)
+println("master")
+println(task())
+
+println("workers")
+println.(outputs)
 
 rmprocs.(workers())
