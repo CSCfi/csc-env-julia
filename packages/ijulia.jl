3d24d7056a8077ab3545a80c2311bb1cd16fc782
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-04-27 10:36:52 +0300

    discard the default julia kernel

--- packages/ijulia.jl
+++ packages/ijulia.jl
@@ -1,12 +1,11 @@
 using Pkg
 
+# We set JUPYTER_DATA_DIR to temporary directory avoid install the default kernel.
+ENV["JUPYTER_DATA_DIR"] = mktempdir()
+
 # Install IJulia
 Pkg.add(name="IJulia", version="1.24.0")
 
 # Pin the version of IJulia to avoid updating it accidentally.
 # We can use Pkg.free to unpin it.
 Pkg.pin("IJulia")
-
-# Remove the default kernel
-jupyter_dir = ENV["JUPYTER_DATA_DIR"]
-rm(joinpath(jupyter_dir, "kernels", "julia-$(VERSION.major).$(VERSION.minor)"); recursive=true, force=true)
