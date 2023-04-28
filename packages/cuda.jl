9c3dfcd333d809823bfe1634de03e155939e588f
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-04-28 08:31:15 +0300

    separate package install scripts

---
+++ packages/cuda.jl
@@ -0,0 +1,8 @@
+using Pkg
+Pkg.add(name="CUDA", version="4.0.1")
+Pkg.pin("CUDA")
+
+using CUDA
+CUDA.set_runtime_version!("local")
+
+Pkg.instantiate()
---
+++ packages/mkl.jl
@@ -0,0 +1,4 @@
+using Pkg
+Pkg.add(name="MKL", version="0.6.0")
+Pkg.pin("MKL")
+Pkg.instantiate()
