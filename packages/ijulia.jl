36f9e2e759ff98583a1e83143b4104320d6eece8
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-04-28 10:57:39 +0300

    ijulia install

--- packages/ijulia.jl
+++ packages/ijulia.jl
@@ -3,12 +3,9 @@ using Pkg
 # We set JUPYTER_DATA_DIR to temporary directory avoid install the default kernel.
 ENV["JUPYTER_DATA_DIR"] = mktempdir()
 
-# Jupyter executable name
+# Set Jupyter executable name to avoid installing Conda.
 ENV["JUPYTER"] = "jupyter"
 
-# Install IJulia
+# Install package
 Pkg.add(name="IJulia", version="1.24.0")
-
-# Pin the version of IJulia to avoid updating it accidentally.
-# We can use Pkg.free to unpin it.
 Pkg.pin("IJulia")
