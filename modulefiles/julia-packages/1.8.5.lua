fe73a0aca62f6cdd5c07703d9b08bf5bd6217316
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-04-28 08:21:52 +0300

    julia on jupyter

--- modulefiles/julia-packages/1.8.5.lua
+++ modulefiles/julia-packages/1.8.5.lua
@@ -1,5 +1,5 @@
 -- Module dependecies for installing packages.
-depends_on("gcc/11", "openmpi/4", "cuda/11", "julia/1.8.5")
+depends_on("julia/1.8.5")
 
 -- We use the default load path by unsetting the environment variable.
 unsetenv("JULIA_LOAD_PATH")
@@ -15,9 +15,3 @@ setenv("JULIA_HISTORY", "/dev/null")
 
 -- Diretory for the Julia kernel
 setenv("JUPYTER_DATA_DIR", "/appl/soft/math/julia/jupyter")
-
--- Jupyter executable name
-setenv("JUPYTER", "jupyter")
-
--- IJulia version 1.24.0 this causes an error. Uncomment when it is fixed.
--- setenv("IJULIA_NODEFAULTKERNEL", "")
--- packages/ijulia.jl
+++ packages/ijulia.jl
@@ -3,6 +3,9 @@ using Pkg
 # We set JUPYTER_DATA_DIR to temporary directory avoid install the default kernel.
 ENV["JUPYTER_DATA_DIR"] = mktempdir()
 
+# Jupyter executable name
+ENV["JUPYTER"] = "jupyter"
+
 # Install IJulia
 Pkg.add(name="IJulia", version="1.24.0")
 
