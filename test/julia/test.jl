b68d1d59280e09ffd374be14667f9c2b8fae2c2a
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-05-17 15:15:13 +0300

    simplified script

--- test/julia/test.jl
+++ test/julia/test.jl
@@ -1,8 +1,5 @@
-# Find Julia application directory
-JULIA_APPLDIR = dirname(Sys.BINDIR)
-
 # Path to the file to that contains tests for Julia's Base and Stdlib
-testfile = joinpath(JULIA_APPLDIR, "share", "julia", "test", "runtests.jl")
+testfile = joinpath(dirname(Sys.BINDIR), "share", "julia", "test", "runtests.jl")
 
 # Include the file to run the tests
 include(testfile)
