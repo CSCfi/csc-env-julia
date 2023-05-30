16bcac466872e6995afdc94861f2ebea83571d19
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-05-30 11:56:29 +0300

    csc appl dir

--- jupyter/install.sh
+++ jupyter/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-JULIA_JUPYTER_ENV_DIR=$1
+JULIA_JUPYTER_ENV_DIR="$CSC_APPL_DIR/soft/math/julia-jupyter/env"
 mkdir -p "$JULIA_JUPYTER_ENV_DIR"
 python3.9 -m venv "$JULIA_JUPYTER_ENV_DIR"
 export PATH="$JULIA_JUPYTER_ENV_DIR/bin:$PATH"
