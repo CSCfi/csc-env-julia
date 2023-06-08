9bfdc1a8736eb0b3b6d6149b38c4f1269822defa
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-06-08 09:07:13 +0300

    bind major versions of dependencies

--- modulefiles/puhti/julia-cuda/1.8.5.lua
+++ modulefiles/puhti/julia-cuda/1.8.5.lua
@@ -1,6 +1,6 @@
 help("Environment for using CUDA.jl with the Julia language.")
 
-depends_on("julia/1.8.5", "cuda")
+depends_on("julia/1.8.5", "cuda/11")
 
 -- Disable memory pool for CUDA.jl
 setenv("JULIA_CUDA_USE_MEMORY_POOL", "none")
