2980c71e6cc6d3a3694e315ff439e49cde15ea40
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-06-12 08:23:42 +0300

    ijulia tests

---
+++ test/ijulia/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Set strict mode
+set -euo pipefail
+
+# Load Julia module
+module --quiet purge
+module load "julia/$JULIA_VERSION"
+module list
+
+# Set Julia project
+export JULIA_PROJECT="$PWD/v$JULIA_VERSION"
+
+# Instantiate the Julia project
+julia -e 'import Pkg; Pkg.instantiate()'
+
+# Run the test set
+julia test.jl
