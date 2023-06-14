ca79dfb556f25de66def35b55b34f7e5a76e7968
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-06-14 11:35:08 +0300

    sanity check

--- test/check/run.sh
+++ test/check/run.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 # Check module loading
 module purge
-module load julia/1.8.5
+module load "julia/$JULIA_VERSION"
 module list
 
 # Check manual availability
 man -w julia
 
 # Check environment
-julia ./check.jl
+julia "./$1.jl"
