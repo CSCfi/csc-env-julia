2b02cdb18081a9f51d0201b43c0eec5c2abd2248
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-06-14 11:42:20 +0300

    log info

--- test/check/puhti.jl
+++ test/check/puhti.jl
@@ -1,33 +1,37 @@
-# Print information about the installation
+@info "Print information about the installation"
 println(VERSION)
 println(DEPOT_PATH)
 println(LOAD_PATH)
 println(Base.load_path())
 println(Base.active_project())
 
-# Check stdlib availability
+@info "Check stdlib availability"
 using Test
 
-# Check that the default environment is the default user location.
+@info "Check that the default environment is the default user location."
 @test Base.active_project() == joinpath(homedir(), ".julia", "environments", "v$(VERSION.major).$(VERSION.minor)", "Project.toml")
 
-# Lets change the default user depot to a clean, temporary one.
+@info "Lets change the default user depot to a clean, temporary one."
 popfirst!(DEPOT_PATH)
 tmp = mktempdir(; cleanup=true)
 pushfirst!(DEPOT_PATH, joinpath(tmp, ".julia"))
 
-# Check that MPI is available as a shared package.
-import MPI
-@test contains(pathof(MPI), "/appl/soft/math/julia/depot")
-
-# Check that IJulia is available as a shared package.
-import IJulia
-@test contains(pathof(IJulia), "/appl/soft/math/julia/depot")
-
-# Check that environments work
+@info "Check that environments work"
 import Pkg
 Pkg.activate(tmp)
 Pkg.compat("julia", string(VERSION))
 Pkg.add("Example")
 Pkg.instantiate()
 import Example
+
+@info "Check that MPI is available as a shared package."
+import MPI
+@test contains(pathof(MPI), "/appl/soft/math/julia/depot")
+
+@info "Check that CUDA is available as a shared package."
+import CUDA
+@test contains(pathof(CUDA), "/appl/soft/math/julia/depot")
+
+@info "Check that IJulia is available as a shared package."
+import IJulia
+@test contains(pathof(IJulia), "/appl/soft/math/julia/depot")
