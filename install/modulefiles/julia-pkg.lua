3f746cdcdbcc863449aba7e947a9864650176515
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-08-01 09:15:20 +0300

    install modulefile for installing packages

---
+++ install/modulefiles/julia-pkg.lua
@@ -0,0 +1,14 @@
+-- Installing packages requires loading the julia module.
+prereq("julia")
+
+-- Use the default load path for installing shared packages.
+pushenv("JULIA_LOAD_PATH", "@:@v#.#:@stdlib")
+
+-- Directory for the shared packages and other depots.
+pushenv("JULIA_DEPOT_PATH", os.getenv("CSC_JULIA_DEPOT_DIR") or "")
+
+-- Directory for the shared environment.
+pushenv("JULIA_PROJECT", os.getenv("CSC_JULIA_ENVIRONMENT_DIR") or "")
+
+-- Disable history when installing shared packages.
+pushenv("JULIA_HISTORY", "/dev/null")
