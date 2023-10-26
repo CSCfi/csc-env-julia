ddaf577655bd674bb7ed4221ad33a205f292cd27
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-10-26 14:33:23 +0300

    julia gpu

--- install/pkg/cuda/5.0.0.jl
+++ install/pkg/cuda/5.0.0.jl
@@ -4,3 +4,6 @@ Pkg.pin("CUDA")
 
 using CUDA
 CUDA.set_runtime_version!(local_toolkit=true)
+#CUDA.set_runtime_version!(v"{{ julia_gpu_version }}", local_toolkit=true)
+#CUDA.set_runtime_version!(v"11.4", local_toolkit=true)
+#CUDA.set_runtime_version!(v"11.7", local_toolkit=true)
