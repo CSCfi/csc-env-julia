ec50e6b131cdb95e2de047118c6aa31a6cdddcf8
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-10-31 16:27:02 +0200

    improved test

--- install/julia-test/clustermanagers/runtests.jl
+++ install/julia-test/clustermanagers/runtests.jl
@@ -1,13 +1,30 @@
+using Test
 using Distributed
 using ClusterManagers
 
 # SLURM_NTASKS = SLURM_NNODES * SLURM_NTASKS_PER_NODE
-ntasks = parse(Int, ENV["SLURM_NTASKS"])
-cpus_per_task = get(ENV, "SLURM_CPUS_PER_TASK", "1")
+const ntasks = parse(Int, ENV["SLURM_NTASKS"])
+const cpus_per_task = get(ENV, "SLURM_CPUS_PER_TASK", "1")
+
 manager = SlurmManager(ntasks)
 ps = addprocs(manager; exeflags="--threads=$cpus_per_task")
-tasks = map(workers()) do worker
+
+@test nprocs() == ntasks + 1
+@test workers() == ps
+
+tasks = map(ps) do worker
     @spawnat worker (myid(), gethostname(), Threads.nthreads())
 end
-println(fetch.(tasks))
-foreach(rmprocs, workers())
+
+rs = fetch.(tasks)
+for (worker, r) in zip(ps, rs)
+    println(r)
+    @test r[1] == worker
+    @test r[3] == cpus_per_task
+end
+
+@test remotecall_fetch(+,ps[1],1,1) == 2
+
+rmprocs(ps)
+@test nprocs() == 1
+@test workers() == [1]
