bfe2a3faa7ebdfcf18130ea8736163c063e14664
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-03-06 13:15:41 +0200

    group name

--- install.sh
+++ install.sh
@@ -24,18 +24,20 @@ shift 3
 
 case $SYSTEMNAME in
     local|localhost)
-        ansible-playbook -i hosts.yaml -l group_localhost -e "version=$VERSION" "$TARGET/install.yaml" "$@"
+        GROUPNAME=group_localhost
         ;;
     puhti)
-        ansible-playbook -i hosts.yaml -l group_puhti -e "version=$VERSION" "$TARGET/install.yaml" "$@"
+        GROUPNAME=group_puhti
         ;;
     mahti)
-        ansible-playbook -i hosts.yaml -l group_mahti -e "version=$VERSION" "$TARGET/install.yaml" "$@"
+        GROUPNAME=group_mahti
         ;;
     lumi)
-        ansible-playbook -i hosts.yaml -l group_lumi -e "version=$VERSION" "$TARGET/install.yaml" "$@"
+        GROUPNAME=group_lumi
         ;;
     *)
         exit 1
         ;;
 esac
+
+ansible-playbook -i hosts.yaml -l "$GROUPNAME" -e "version=$VERSION" "$TARGET/install.yaml" "$@"
