0a89ac10f6524e8699b5adb41e0d44c31097f8d6
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-03-18 11:30:07 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
-usage="
+USAGE="
 Install Julia and related environments using Ansible.
 
 USAGE
-    ./install.sh <systemname> <target> <version>
+    install.sh <systemname> <target> <version>
 
 EXAMPLES
     Install Julia version 1.10.2 to Puhti:
 
-    ./install.sh puhti julia 1.10.2
+    install.sh puhti julia 1.10.2
 
     Install MPI preferences for version 0.20 to Puhti:
 
-    ./install.sh puhti mpi 0.20
+    install.sh puhti mpi 0.20
 
     Install CUDA preferences for version 5.2 to Puhti:
 
-    ./install.sh puhti cuda 5.2
+    install.sh puhti cuda 5.2
 
     Install ADMGPU preferences for version 0.8 to LUMI:
 
-    ./install.sh lumi amdgpu 0.8
+    install.sh lumi amdgpu 0.8
 "
 
 case ${1:-} in
@@ -38,12 +38,12 @@ case ${1:-} in
     lumi)
         GROUPNAME=group_lumi
         ;;
-    -h|--help)
-        echo "$usage"
+    help|-h|--help)
+        echo "$USAGE"
         exit 0
         ;;
     *)
-        echo "$usage" >&2
+        echo "$USAGE" >&2
         exit 1
         ;;
 esac
