9b92102708a19f843c93c3987a8123cc4657863c
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-12-03 15:01:18 +0200

    convert install.sh to argc

---
+++ Argcfile.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+
+# @cmd
+# @option --host![=localhost|puhti|mahti|lumi]
+# @option --version!
+# @option --arch=linux-x86_64
+install_julia() {
+    ansible-playbook \
+        -i hosts.yaml \
+        -l "group_${argc_host}" \
+        -e "version=${argc_version}" \
+        -e "arch=${argc_arch}" \
+        "./julia/install.yaml" "$@"
+}
+
+# @cmd
+# @option --host![puhti|mahti|lumi]
+# @option --version!
+install_mpi() {
+    ansible-playbook \
+        -i hosts.yaml \
+        -l "group_${argc_host}" \
+        -e "version=${argc_version}" \
+        "./mpi/install.yaml" "$@"
+}
+
+# @cmd
+# @option --host![puhti|mahti]
+# @option --version!
+install_cuda() {
+    ansible-playbook \
+        -i hosts.yaml \
+        -l "group_${argc_host}" \
+        -e "version=${argc_version}" \
+        "./cuda/install.yaml" "$@"
+}
+
+# @cmd
+# @option --host![lumi]
+# @option --version!
+install_amdgpu() {
+    ansible-playbook \
+        -i hosts.yaml \
+        -l "group_${argc_host}" \
+        -e "version=${argc_version}" \
+        "./amdgpu/install.yaml" "$@"
+}
+
+# See more details at https://github.com/sigoden/argc
+eval "$(argc --argc-eval "$0" "$@")"
