f76cb641ae5c235155b0a4fd5b94a6abe8d2ce54
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-12-19 15:37:50 +0200

    refactoring

--- benchmark/mpi/puhti.sh
+++ benchmark/mpi/puhti.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-JULIA_VERSION=$1
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
-export JULIA_VERSION SCRIPT_DIR
 
-sbatch \
-    --account="$SBATCH_ACCOUNT" \
-    --output="benchmark_mpi_%j.out" \
-    --job-name=benchmark_mpi \
-    --partition=test \
-    --time=00:15:00 \
-    --nodes=2 \
-    --ntasks-per-node=1 \
-    --cpus-per-task=1 \
-    --mem-per-cpu=8000 \
-    "$SCRIPT_DIR/batch.sh"
+sbatch <<EOF
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --output=benchmark_julia_mpi_%j.out
+#SBATCH --job-name=benchmark_julia_mpi
+#SBATCH --partition=test
+#SBATCH --time=00:15:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8000
+module load julia
+module load julia-mpi
+module list
+julia "$SCRIPT_DIR/benchmark.jl"
+EOF
