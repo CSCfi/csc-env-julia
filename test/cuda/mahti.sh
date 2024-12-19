4b5d6847c8ab07c5f427e15db5bc62cbd90adbc5
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-12-19 14:12:56 +0200

    refactoring

--- test/cuda/mahti.sh
+++ test/cuda/mahti.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-JULIA_VERSION=$1
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
-export JULIA_VERSION SCRIPT_DIR
 
-sbatch \
-    --account="$SBATCH_ACCOUNT" \
-    --output="test_cuda_%j.out" \
-    --job-name="test_cuda" \
-    --partition=gpusmall \
-    --time=01:00:00 \
-    --nodes=1 \
-    --ntasks-per-node=1 \
-    --cpus-per-task=32 \
-    --mem-per-cpu=1750 \
-    --gres=gpu:a100:1 \
-    "$SCRIPT_DIR/batch.sh"
+sbatch <<EOF
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --output=test_julia_cuda_%j.out
+#SBATCH --job-name=test_julia_cuda
+#SBATCH --partition=gpusmall
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=1750
+#SBATCH --gres=gpu:a100:1
+module load julia
+module load julia-cuda
+module list
+julia "$SCRIPT_DIR/runtests.jl"
+EOF
--- test/cuda/puhti.sh
+++ test/cuda/puhti.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-JULIA_VERSION=$1
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
-export JULIA_VERSION SCRIPT_DIR
 
-sbatch \
-    --account="$SBATCH_ACCOUNT" \
-    --output="test_cuda_%j.out" \
-    --job-name="test_cuda" \
-    --partition=gpu \
-    --time=01:00:00 \
-    --nodes=1 \
-    --ntasks-per-node=1 \
-    --cpus-per-task=10 \
-    --mem-per-cpu=4000 \
-    --gres=gpu:v100:1 \
-    "$SCRIPT_DIR/batch.sh"
+sbatch <<EOF
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --output=test_julia_cuda_%j.out
+#SBATCH --job-name=test_julia_cuda
+#SBATCH --partition=gpu
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=4000
+#SBATCH --gres=gpu:v100:1
+module load julia
+module load julia-cuda
+module list
+julia "$SCRIPT_DIR/runtests.jl"
+EOF
