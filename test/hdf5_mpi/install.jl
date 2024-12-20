b4eebe10c0bcbbf40b272d87dd54905bd820616e
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-12-20 11:30:28 +0200

    install

--- test/hdf5_mpi/install.jl
+++ test/hdf5_mpi/install.jl
@@ -1,3 +1,9 @@
+# Install LocalPreferences.toml for HDF5 parallel.
+#
+# $ module use /appl/local/csc/modulefiles
+# $ module load julia julia-mpi cray-hdf5-parallel
+# $ julia --threads 8 ./install.jl
+
 using Pkg
 Pkg.add("Preferences")
 Pkg.add("HDF5")
