518831eb04dbe0740cd1eb263c2068f5097d36f6
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-17 14:36:58 +0300

    roihu mpi test

---
+++ test/mpi/runtests-roihu_cpu.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(realpath "$(dirname "$0")")"
+
+sbatch <<EOF
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --output=test_julia_mpi_%j.out
+#SBATCH --job-name=test_julia_mpi
+#SBATCH --partition=medium
+#SBATCH --time=00:15:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8000
+export TMPDIR=$PWD/tmp
+export UCX_WARN_UNUSED_ENV_VARS=n
+module purge
+module load julia
+module load julia-mpi
+module list
+julia --project=. runtests.jl
+EOF
