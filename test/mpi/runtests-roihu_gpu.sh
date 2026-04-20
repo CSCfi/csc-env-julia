9361e26e407ba0ae391c4b661b7d398a0861783e
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-20 09:42:34 +0300

    tests

--- test/mpi/runtests-roihu_gpu.sh
+++ test/mpi/runtests-roihu_gpu.sh
@@ -8,12 +8,13 @@ sbatch <<EOF
 #SBATCH --account=project_2001659
 #SBATCH --output=test_julia_mpi_%j.out
 #SBATCH --job-name=test_julia_mpi
-#SBATCH --partition=small
+#SBATCH --partition=gputest
 #SBATCH --time=00:15:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8000
+#SBATCH --gpus-per-node=1
 export TMPDIR=/dev/shm
 export UCX_WARN_UNUSED_ENV_VARS=n
 module purge
