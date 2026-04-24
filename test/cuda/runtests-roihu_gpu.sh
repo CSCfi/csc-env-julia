1eead01ca73d4db187f6e990853143e19716dff7
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-04-24 09:07:53 +0300

    roihu gpu cuda

---
+++ test/cuda/runtests-roihu_gpu.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(realpath "$(dirname "$0")")"
+
+sbatch <<EOF
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --output=test_julia_cuda_%j.out
+#SBATCH --job-name=test_julia_cuda
+#SBATCH --partition=gpumedium
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=72
+#SBATCH --mem=120G
+#SBATCH --gpus-per-node=1
+export TMPDIR=/dev/shm
+export UCX_WARN_UNUSED_ENV_VARS=n
+module purge
+module load julia
+#module load julia-cuda
+module list
+julia --project=. runtests.jl
+EOF
