e87b905d75a9330cd6a62cc3a8961c3824102a3b
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-05-04 10:16:07 +0300

    slurmclustermanagers

---
+++ test/slurmclustermanager/roihu_cpu/runtests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(realpath "$(dirname "$0")")"
+
+sbatch <<EOF
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --output=test_julia_slurmclustermanager_%j.out
+#SBATCH --job-name=test_julia_slurmclustermanager
+#SBATCH --partition=test
+#SBATCH --time=00:05:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=384
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=0
+module purge
+module load julia
+module list
+julia --project=. runtests.jl
+EOF
